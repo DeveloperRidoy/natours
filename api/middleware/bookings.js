25ed82791bdab4dadc8de766d74f44dc24ed0881
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-04-02 20:26:54 +0600

    fixing bugs

--- api/middleware/bookings.js
+++ api/middleware/bookings.js
@@ -3,6 +3,7 @@ const User = require("../../mongodb/models/user");
 
 exports.createBookingCheckout = async (session) => {
     try {
+        console.log(session);
         const tour = session.client_reference_id;
         const user = (await User.find({ email: session.customer_details.email })).id;
         const price = session.amount_total / 100;
