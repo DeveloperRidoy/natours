34ff4797ae4dba430f5f14e6300888e87ca0f2b3
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-04-02 20:56:27 +0600

    fixing bugs

--- api/middleware/bookings.js
+++ api/middleware/bookings.js
@@ -8,6 +8,6 @@ exports.createBookingCheckout = async (session, res) => {
         const price = session.display_items[0].amount / 100;
         await Booking.create({ tour, user, price });
     } catch (error) {
-        return res.status(500).json({ status: 'fail', message: `webhook error: ${error.message}` });
+        return res.status(500).json({ status: 'fail', message: `webhook error: ${error.message}`, tour, user, price });
     }
 }
