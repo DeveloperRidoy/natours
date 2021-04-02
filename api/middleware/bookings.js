8d3bdd2f7d648cec73bec605aedc61d252bc6173
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-04-02 21:18:28 +0600

    fixing bugs

--- api/middleware/bookings.js
+++ api/middleware/bookings.js
@@ -4,10 +4,9 @@ const User = require("../../mongodb/models/user");
 exports.createBookingCheckout = async (session, res) => {
     try {
         const tour = session.client_reference_id;
-        const user = (await User.find({ email: session.customer_email })).id;
+        const user = (await User.findOne({ email: session.customer_email })).id;
         const price = session.display_items[0].amount / 100;
-        // await Booking.create({ tour, user, price });
-        return res.json({ status: 'success', data: {tour, user, price}});
+        await Booking.create({ tour, user, price });
     } catch (error) {
         return res.status(500).json({ status: 'fail', message: `webhook error: ${error.message}`});
     }
